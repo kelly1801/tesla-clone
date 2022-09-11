cdfd46bdcb51cd0b1a41fdec97f7f6a2f6817216
¨Kelly <fabianachara@gmail.com>
2022-09-11 16:43:59 -0500

    add initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Header from './Components/Header'
+import Home from './Components/home'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+  
+      <Home/>
+    </div>
+  )
+}
+
+export default App
