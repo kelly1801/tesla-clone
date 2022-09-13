f74c03215b83c699341e1ae7911623cca2c3b696
¨Kelly <fabianachara@gmail.com>
2022-09-12 22:33:24 -0500

    add collapsable menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import Header from './Components/Header'
 import Home from './Components/home'
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import styled from "styled-components";
 import MenuIcon from '@mui/icons-material/Menu';
-
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Header() {
+  const [menuOpen, setMenuOpen] = useState(false)
+  
+
+  const cars = useSelector(selectCars)
+  alert(cars)
   return (
     <Container>
 
@@ -28,10 +33,40 @@ function Header() {
 <li><a href="#">Account</a></li>
 <li><a href="#">Menu</a></li>
 
-<CustomMenu />
+<CustomMenu onClick={() => setMenuOpen(true)}/>
         </List>
+    {
+      menuOpen &&
+       <BurguerNav >
     
-    
+    <CloseBtn onClick={() => setMenuOpen(false)}/>
+
+
+       <li><a href='#'>Model S</a></li>
+        <li><a href='#'>Model 3</a></li>
+        <li><a href='#'>Model X</a></li>
+        <li><a href='#'>Model Y</a></li>
+        <li><a href='#'>Solar Roof</a></li>
+        <li><a href='#'>Solar Panels</a></li>
+        <li><a href='#'>Existing Inventory</a></li>
+        <li><a href='#'>Used Inventory</a></li>
+        <li><a href='#'>Trade-in</a></li>
+        <li><a href='#'>Test Drive</a></li>
+        <li><a href='#'>Insurance</a></li>
+        <li><a href='#'>Powerwall</a></li>
+        <li><a href='#'>Commercial Energy</a></li>
+        <li><a href='#'>Utilities</a></li>
+       <li><a href='#'>Charging</a></li>
+        <li><a href='#'>Find Us</a></li>
+        <li><a href='#'>Support</a></li>
+        <li><a href='#'>Investor Relations</a></li>
+        <li><a href='#'>Shop</a></li>
+        <li><a href='#'>Account</a></li>
+        <li><a href='#'>More</a></li>
+      
+
+    </BurguerNav >
+    }
 
     </Container>
     
@@ -49,6 +84,7 @@ align-items: center;
 padding: 0 1.2rem;
 justify-content: space-between;
 width: 100%;
+z-index: 1;
 
 `
 const Menu = styled.div`
@@ -71,5 +107,27 @@ align-items:center;
 `
 const CustomMenu = styled(MenuIcon)`
 
+cursor: pointer;
+`
+
+const BurguerNav = styled.ul`
+background-color: white;
+position: fixed;
+top: 0;
+bottom: 0;
+right: 0;
+width: 20rem;
+padding: 1.5rem 2rem;
+z-index: 2;
+transition: all 2s ease;
+
+li:hover{
+  background-color:  #f2f2f2;
+}
+`
+const CloseBtn = styled(CloseIcon)`
+position: absolute;
+right: 1rem;
+top: 10px;
 cursor: pointer;
 `
